f0ae9fece183612c329a39e5fdfe8128902dac1a
qiaoduan <qiaoduan@qunhemail.com>
2021-03-30 20:15:59 +0800

    release and debug build

--- build_flutter_android/app/src/main/java/com/meijian/build_flutter_android/MainActivity.java
+++ build_flutter_android/app/src/main/java/com/meijian/build_flutter_android/MainActivity.java
@@ -3,6 +3,9 @@ package com.meijian.build_flutter_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    findViewById(R.id.go_flutter).setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        startActivity(FlutterActivity.createDefaultIntent(MainActivity.this));
+      }
+    });
   }
 }
